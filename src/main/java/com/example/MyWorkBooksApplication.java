aaec9219951147ffd023732535a6f75ccd7f4570
yusukematsumoto <93502995+yusuke-rakus@users.noreply.github.com>
2021-11-14 21:16:47 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/MyWorkBooksApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkBooksApplication.class, args);
+	}
+
+}
